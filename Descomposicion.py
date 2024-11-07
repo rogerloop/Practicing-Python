02e6d9dc3aa5f70c7fa6fd983bc4ecfc5cc66248
tu-usuario <tu-email@example.com>
2024-11-07 22:12:08 +0100

    V1.0

---
+++ Descomposicion.py
@@ -0,0 +1,15 @@
+""" Realiza una función que realice la descomposición en
+factores de un número. Deberá devolver una lista con
+def factorial(x):
+if x>1:
+los factores de dicho número. Recordad que la
+return x*factorial(x-1)
+descomposición en factores de un número consiste
+else:
+en hallar el conjunto de números primos cuya
+return 1
+multiplicación dé dicho número como resultado. 
+"""
+
+
+
