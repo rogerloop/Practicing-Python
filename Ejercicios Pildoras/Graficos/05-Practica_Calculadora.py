b1a69b51a4e84e70169df45448f164cf697cf99a
GitHub Desktop RGR <bascular-rendiment0x@icloud.com>
2025-02-13 00:39:04 +0100

    Añadir operaciones

--- Ejercicios Pildoras/Graficos/05-Practica_Calculadora.py
+++ Ejercicios Pildoras/Graficos/05-Practica_Calculadora.py
@@ -6,6 +6,10 @@ miFrame=Frame(raiz, padx=10, pady=10)
 
 miFrame.pack()
 
+operacion=""
+
+resultadoParcial=0
+
 #----------Pantalla------------------
 
 numeroPantalla=StringVar()
@@ -14,12 +18,48 @@ numeroPantalla=StringVar()
 pantalla=Entry(miFrame, textvariable=numeroPantalla)
 pantalla.grid(row=1, column=1, padx=10, pady=10, columnspan=4)
 pantalla.config(background="black", fg="#03f943", justify="right")
+numeroPantalla.set("0")
 
 #-------pulsaciones teclado-------------
 
 def numeroPulsado(num):
 
-    numeroPantalla.set(numeroPantalla.get() + num)
+    global operacion
+
+    if operacion!="":
+        numeroPantalla.set(num)
+        operacion=""
+    else:    
+        # Verificar si el número ya contiene un punto decimal
+        if num == "." and "." in numeroPantalla.get():
+            return
+        numeroPantalla.set(numeroPantalla.get() + num)
+
+
+#----------ffuncion suma------------------
+
+def suma(num):
+
+    global operacion
+
+    global resultadoParcial
+
+    resultadoParcial+=float(num)
+
+    operacion="suma"
+
+    numeroPantalla.set(resultadoParcial)
+
+#----------funcion resultado------------------
+
+def resultado():
+
+    global resultadoParcial
+
+    numeroPantalla.set(resultadoParcial+float(numeroPantalla.get()))
+
+    resultadoParcial=0
+
 
 #-----------fila 1-----------------
 
@@ -60,9 +100,9 @@ boton0=Button(miFrame, text="0", width=3, command=lambda:numeroPulsado("0"))
 boton0.grid(row=5, column=1)
 botonComa=Button(miFrame, text=",", width=3, command=lambda:numeroPulsado("."))
 botonComa.grid(row=5, column=2)
-botonIgual=Button(miFrame, text="=", width=3)
+botonIgual=Button(miFrame, text="=", width=3, command=lambda:resultado())
 botonIgual.grid(row=5, column=3)
-botonSum=Button(miFrame, text="+", width=3)
+botonSum=Button(miFrame, text="+", width=3, command=lambda:suma(numeroPantalla.get()))
 botonSum.grid(row=5, column=4)
 
 
